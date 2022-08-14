9e57155c55a2fa67ec083e3beb3cd7d2e237adba
William <xiwu9085@uni.sydney.edu.au>
2022-08-14 18:01:44 +1000

    -AUTO- Sun Aug 14 18:01:44 AEST 2022

--- week3/run_perf_qn.bash
+++ week3/run_perf_qn.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-gcc perf_qn.c -ggdb3 || exit
+gcc perf_qn.c -ggdb3 -o program || exit
 /home/blepers/linux-huge/tools/perf/perf record -F 100 -a -g -- ./program
 /home/blepers/linux-huge/tools/perf/perf script >stackdata.perf
 ../../FlameGraph/stackcollapse-perf.pl stackdata.perf >stackdata.folded
