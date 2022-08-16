9a91d2b34710ab5dc6bfc4e7a29eaab20d4fe2ed
William <xiwu9085@uni.sydney.edu.au>
2022-08-16 16:45:58 +1000

    -AUTO- Tue Aug 16 16:45:58 AEST 2022

---
+++ week4/false_sharing.c
@@ -0,0 +1,58 @@
+//
+// Created by Xiaoxiang Wu on 16/8/2022.
+//
+
+#include "../main.h"
+#include <pthread.h>
+
+#define T 8
+
+struct thread_data {
+    int *result;
+    int result_index;
+    int *numbers;
+    int numbers_len;
+};
+
+void *worker(void *data) {
+    struct thread_data *d = (struct thread_data *) data;
+    for (int i = 0; i < d->numbers_len; i++) {
+        d->result[d->result_index] += d->numbers[i];
+    }
+    return NULL;
+}
+
+int main() {
+
+    init_seed();
+    int numbers_len = 300000000;
+    int *numbers = malloc(numbers_len * sizeof(int));
+    for (int i = 0; i < numbers_len; i++) {
+        numbers[i] = (int) lehmer64();
+    }
+
+    pthread_t threads[T];
+    struct thread_data datas[T];
+    int result[T];
+
+    for (int i = 0; i < T; i++) {
+        datas[i].result = result;
+        datas[i].result_index = i;
+        datas[i].numbers = numbers;
+        datas[i].numbers_len = numbers_len;
+    }
+
+    for (int i = 0; i < T; i++) {
+        pthread_create(threads + i, NULL, worker, datas + i);
+    }
+
+    int sum = 0;
+    for (int i = 0; i < T; i++) {
+        pthread_join(threads[i], NULL);
+        sum += result[i];
+    }
+
+    printf("sum: %d", sum);
+
+    return 0;
+}
