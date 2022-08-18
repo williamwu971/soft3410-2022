c596ddebb7521afc317f31f7c6661f4bde6e8974
William <xiwu9085@gmail.com>
2022-08-18 15:58:08 +1000

    -AUTO- Thu Aug 18 03:58:08 PM AEST 2022

--- week4/graph_false_sharing.bash
+++ week4/graph_false_sharing.bash
@@ -1,10 +1,22 @@
 #!/usr/bin/env bash
 
+make || exit
 rm -f data.txt
+rm -f ./*.png
 
 for i in {1..8}; do
   printf '%s ' "$i" >>data.txt
   ./false_sharing.out "$i" 2>>data.txt || exit
 done
 
-gnuplot <gnuplot.txt
+sed 's/PLACE/same/g' gnuplot.txt | gnuplot
+
+rm -f data.txt
+
+for i in {1..8}; do
+  printf '%s ' "$i" >>data.txt
+  ./no_false_sharing.out "$i" 2>>data.txt || exit
+done
+
+sed 's/PLACE/diff/g' gnuplot.txt | gnuplot
+echo "done"
