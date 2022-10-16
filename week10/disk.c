ed660bb75942ed3ba02d4bf5f07fe2614e8346d4
William <xiwu9085@gmail.com>
2022-10-16 23:09:43 +1100

    -AUTO- Sun Oct 16 11:09:43 PM AEDT 2022

--- week10/disk.c
+++ week10/disk.c
@@ -14,17 +14,18 @@
 #include <string.h>
 #include <time.h>
 #include <sys/time.h>
+#include <omp.h>
 
 
 int main(int argc, char **argv) {
 
-    long len = 1024 * 1024 * 1024L;
-    long nb_accesses = 10000;
+    long len = 16 * 1024 * 1024L;
+    long nb_accesses = 1000000;
     long granularity = 64;
 
-    int fd = open("disk_pool", O_RDWR | O_DIRECT | O_CREAT, 0777);
-    lseek(fd, len, SEEK_SET);
-    assert(write(fd, "a", 1) == 1);
+    int fd = open("disk_pool", O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+    assert(lseek(fd, len, SEEK_SET) == len);
+    assert(write(fd, "a", 1) != -1);
 
     char *map = mmap(NULL, len, PROT_READ | PROT_WRITE, MAP_SHARED_VALIDATE, fd, 0);
     assert(map != MAP_FAILED);
@@ -34,7 +35,7 @@ int main(int argc, char **argv) {
     long *locations = malloc(nb_accesses * sizeof(long));
     srand(time(NULL));
 
-    if (strcasestr(argv[1], "r")) {
+    if (argc != 1 && strcasestr(argv[1], "r")) {
         puts("random");
 
         for (long i = 0; i < nb_accesses; i++) {
@@ -55,17 +56,28 @@ int main(int argc, char **argv) {
         data[i] = rand();
     }
 
+    omp_set_num_threads(8);
+
     u_int64_t elapsed;
     struct timeval st, et;
 
     gettimeofday(&st, NULL);
 
-#pragma omp parallel for
-    for (long i = 0; i < nb_accesses; i++) {
-        long loc = locations[i];
-        memcpy(map + loc, data, granularity);
-        msync(map + loc, granularity, MS_SYNC);
+#pragma omp parallel
+    {
+
+        long start = nb_accesses / omp_get_max_threads() * omp_get_thread_num();
+        long end = nb_accesses / omp_get_max_threads() * (omp_get_thread_num() + 1);
+
+        printf("thread %d start %ld end %ld\n", omp_get_thread_num(), start, end);
+
+        for (long i = start; i != end; i++) {
+            long loc = locations[i];
+            memcpy(map + loc, data, granularity);
+            msync(map + loc, granularity, MS_SYNC);
+        }
     }
+
 
     gettimeofday(&et, NULL);
     elapsed = ((et.tv_sec - st.tv_sec) * 1000000) + (et.tv_usec - st.tv_usec) + 1;
